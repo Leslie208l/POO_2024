a0f4ad38b6f943d2d3362f47932e5b3648c45b36
Leslie208l <lesliesosa080305@gmail.com>
2024-05-31 11:31:47 -0600

    j

--- examen/examen.py
+++ examen/examen.py
@@ -6,3 +6,10 @@ print("N:",N)
 print("H:",H)
 print("D:",D)
 print("S:",S)
+#Calcular e imprimir en pantalla el sueldo que gana a la semana
+#Calcular e imprimir el sueldo al mes,asi como una leyenda que diga:"Obrero tipo A",
+#Dependiendo si el sueldo mensual  es menor o igual a $10,000
+ObreroA=
+ObreroB=
+ if Sueldo mensual <= $10,000
+ if sueldo >=$10,000
---
+++ examen/imc.py
@@ -0,0 +1,12 @@
+Pe="Peso inferior al normal"
+n="normal"
+pes="peso superior al normal"
+o="obesidad"
+print("pe:",pe)
+print("n:",n)
+print("pes:",pes)
+print("o:",o)
+Pe<= 18.5
+n 18.5-24.9
+pes 25.0-29.9
+o > 30.0
