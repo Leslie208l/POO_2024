eac6408de5e91abc4f4868bfe77e771aa8edcfa0
Leslie208l <lesliesosa080305@gmail.com>
2024-06-03 12:13:19 -0600

    a

--- Parcial 1/6_Ejercicios_repaso_1al5/Calculadora_basica.py
+++ Parcial 1/6_Ejercicios_repaso_1al5/Calculadora_basica.py
@@ -24,3 +24,34 @@ def operacionAritmetica(num1,num2,ope):
     if opcion=="1" or opcion=="+" or opcion=="SUMA":
     return f"{n1}+{n2}={n1+n2}"
     elif
+
+
+
+    def solicitarNumeros():
+  global n1,n2  
+  n1=int(input("Numero #1: "))
+  n2=int(input("Numero #2: "))
+  
+
+def operacionAritmetica(num1,num2,opcion):  
+    if opcion=="1" or opcion=="+" or opcion=="SUMA":
+      return f"{n1}+{n2}={n1+n2}"
+    elif opcion=="2" or opcion=="-" or opcion=="RESTA":
+     return f"{n1}-{n2}={n1-n2}"
+    elif opcion=="3" or opcion=="*" or opcion=="MULTIPLICACION":
+     return f"{n1}*{n2}={n1*n2}"
+    elif opcion=="4" or opcion=="/" or opcion=="DIVISION":
+     return f"{n1}/{n2}={n1/n2}"  
+    else:  
+     opcion=False    
+     return "Terminaste la ejecucion del SW"
+
+   system("clear") 
+opcion=True    
+while opcion:
+ print("\n\t..::: CALCULADORA BÁSICA :::... \n 1.- Suma \n 2.- Resta \n 3.- Multiplicacion \n 4.- División \n 5.- SALIR ")
+ opcion=input("\t Elige una opción: ").upper()
+
+if opcion!=5:
+ solicitarNumeros()
+ print(operacionAritmetica(n1,n2,opcion))
---
+++ Parcial 1/6_Ejercicios_repaso_1al5/Parcial2/8_listas/listas.py
@@ -0,0 +1,42 @@
+
+"""
+List (array)
+son colecciones o conjuntos de datos/valores bajo 
+un mismo nombre,para acceder a los valores se 
+hace con un indice numerico
+
+note:sus valores si son modificables
+
+La lista es una coleccion ordenada y modificable,..
+permite miembros duplicados
+"""
+
+#ejemplo crear una lista de numeros e imprimir el contenido
+
+numeros=23
+numeros=23
+
+numeros[23,24 ]
+print(numeros)
+#recorrer la lista con ciclo for
+for i in numeros:
+    print(i)
+
+    #recorrer la lista con el ciclo while
+    i=0
+    tamaño =en(numeros)
+    print(tamaño)
+    i=0
+    while i<=len(numeros):
+        print(numeros(i))
+        1+1
+
+#ejemplo 2 crear una lista de palabras y posteriormente buscar la coincidencia de una palabra
+
+palabras=[ "naranja","utd","america","azul"]
+
+palabra_buscar=input("ingresa la palabra a buscar:")
+
+for i in palabras:
+    if i=palabra_buscar:
+       print ("")
