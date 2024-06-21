f4bf4f824d9666d88b94b7dfde1c511e165ea990
Leslie208l <lesliesosa080305@gmail.com>
2024-06-21 10:08:35 -0600

    q

---
+++ Phyton/Formato de texto.py
@@ -0,0 +1,1 @@
+print("potencia:",7**2)
---
+++ Phyton/Listas.py
@@ -0,0 +1,12 @@
+#Una lista es una secuencia de datos ordenados, la posición importa,
+#heterogénea, puede contener distintos tipos de datos y mutable
+#podemos cambiar la secuencia, ya sea modificando eliminando o añadiendo elementos.
+#Para crear una lista se usan corchetes
+numeros=[]
+cadenas=[]
+print("numeros")
+print("cadenas")
+#Entre ellos están el método "append", que nos permite agregar un elemento al final
+#El método "count", que nos devuelve el número de veces que se repite un elemento
+#El método "index", que nos devuelve la posición de la primera aparición de un elemento
+#El método "remove" que elimina la primera aparición de un elemento.
