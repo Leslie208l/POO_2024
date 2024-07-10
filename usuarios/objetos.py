acc7413b166f95b7a51dd0693194a0190b238820
Leslie208l <lesliesosa080305@gmail.com>
2024-07-10 10:24:53 -0600

    commit

---
+++ usuarios/objetos.py
@@ -0,0 +1,8 @@
+from clases import *
+
+cliente1=Clientes('Ana Torres Guzman','Col. centro 1500 nte','6181234567','1234')
+
+empleado1=Empleados('Daniel Fuentes Loera','Fraac. Alamedas 1300 nte','6183335678',1200.90,123,'A')
+
+cliente1.info_usuario()
+empleado1.info_usuario()
