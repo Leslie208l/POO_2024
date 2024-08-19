78caab247f77003a81a199f85250602aedb52e9e
Leslie208l <lesliesosa080305@gmail.com>
2024-08-18 22:32:05 -0600

    Delete Proyecto Final directory

--- Proyecto Final/db_config.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def configure_database():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",  # Cambia esto si tienes una contraseña
-        database="hotel"
-    )
